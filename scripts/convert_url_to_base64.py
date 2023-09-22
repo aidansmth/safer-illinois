360498bbe7a7e1c8e04724c26f86e2b83ef08007
Aidan Smith <82855280+aidansmth@users.noreply.github.com>
2023-09-22 10:02:27 -0700

    Added supporters grid.

--- scripts/convert_url_to_base64.py
+++ scripts/convert_url_to_base64.py
@@ -1,14 +1,33 @@
 import base64
 import os
 
-url = "https://touchstoneclimbing.com/dogpatch-boulders/"
+url = "https://www.livablecity.org/"
 encoded_url = base64.urlsafe_b64encode(url.encode()).decode()
 
 # You may need to truncate or modify the encoded URL to fit within the character limit.
 # Then, you can create your filename.
-filename = f"{encoded_url}.svg"
+filename = f"{encoded_url}.png"
 
-old_filename = "dogpatch-boulders.svg"
+# Get the list of files in the current directory with .png or .svg extensions
+matching_files = [filename for filename in os.listdir() if filename.endswith(('.png', '.svg'))]
+
+old_filename = ""
+
+# Check if there are any matching files before trying to access the first one
+if matching_files:
+    old_filename = matching_files[0]
+    # Do something with old_filename
+else:
+    print("No .png or .svg files found in the current directory.")
 
 # Rename file old_filename to filename
 os.rename(old_filename, filename)
+
+# Check if file exists where we want to move it
+if os.path.isfile(f"../content/en/supporters/images/{filename}"):
+    print(f"File {filename} already exists in ../content/en/supporters/images/")
+    # Exit the script
+    exit()
+
+# Move file to ../content/en/supporters/images/
+os.replace(filename, f"../content/en/supporters/images/{filename}")
